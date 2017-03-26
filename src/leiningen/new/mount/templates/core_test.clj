ffef0993711c36ef58d91af0fb766d28b60b7a0a
Stuart White <s.white.1@elsevier.com>
2017-03-26 11:22:52 +0100

    Remove spurious square brackets in test template

--- src/leiningen/new/mount/templates/core_test.clj
+++ src/leiningen/new/mount/templates/core_test.clj
@@ -3,6 +3,6 @@
     [clojure.test :refer :all]
     [{{main-ns}}.core :refer :all]))
 
-(deftest add-one-given-value-then-increments []
+(deftest add-one-given-value-then-increments
   (is (= 24 (add-one 23))))
 
